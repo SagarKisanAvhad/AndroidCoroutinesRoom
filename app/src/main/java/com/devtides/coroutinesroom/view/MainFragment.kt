4efab77731333edf5c18c22d613e6c9c1de39f8b
sameer avhad <sameeravhad119@gmail.com>
2020-11-04 21:01:10 +0530

    added coroutine. communicated with database. signup and main fragment.

--- app/src/main/java/com/devtides/coroutinesroom/view/MainFragment.kt
+++ app/src/main/java/com/devtides/coroutinesroom/view/MainFragment.kt
@@ -5,13 +5,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
 import com.devtides.coroutinesroom.R
+import com.devtides.coroutinesroom.model.LoginState
 import com.devtides.coroutinesroom.viewmodel.MainViewModel
 import kotlinx.android.synthetic.main.fragment_main.*
 
@@ -29,6 +28,7 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        usernameTV.text = LoginState.user?.username
         signoutBtn.setOnClickListener { onSignout() }
         deleteUserBtn.setOnClickListener { onDelete() }
 
@@ -39,20 +39,24 @@ class MainFragment : Fragment() {
     fun observeViewModel() {
         viewModel.signout.observe(this, Observer {
 
+            if (it) {
+                val action = MainFragmentDirections.actionGoToSignup()
+                Navigation.findNavController(usernameTV).navigate(action)
+            }
+
         })
         viewModel.userDeleted.observe(this, Observer {
-
+            val action = MainFragmentDirections.actionGoToSignup()
+            Navigation.findNavController(usernameTV).navigate(action)
         })
     }
 
     private fun onSignout() {
-        val action = MainFragmentDirections.actionGoToSignup()
-        Navigation.findNavController(usernameTV).navigate(action)
+        viewModel.onSignout()
     }
 
     private fun onDelete() {
-        val action = MainFragmentDirections.actionGoToSignup()
-        Navigation.findNavController(usernameTV).navigate(action)
+        viewModel.onDeleteUser()
     }
 
 }
